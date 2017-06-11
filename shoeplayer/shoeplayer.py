314c2d7552d12c474a5399126b41a2019d8a8fa8
root <root@debian.aartea.co>
2017-06-11 13:07:09 -0700

    Decorate, try to make it look pretty

--- shoeplayer/shoeplayer.py
+++ shoeplayer/shoeplayer.py
@@ -36,6 +36,20 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
 # launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
